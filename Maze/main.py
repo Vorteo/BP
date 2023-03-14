655218d8e90d1f2a151fec66ef20fd50c9153c92
Vorteo <92757545+Vorteo@users.noreply.github.com>
2023-03-14 17:53:17 +0100

    Add files via upload

--- Maze/main.py
+++ Maze/main.py
@@ -2,6 +2,12 @@ from pyMaze import maze, COLOR, agent, textLabel
 import random
 import math
 
+NUM_MOVES = 200
+NUM_AGENTS = 400
+
+MUTATION_RATE = 0.2
+SELECTION_CUTOFF = 0.1
+
 
 def generate_random_moves(num_of_steps):
     # right, left, up, down
@@ -14,13 +20,34 @@ def calculate_distance(x1, x2):
     return dist
 
 
+def crossover():
+    pass
+
+
+def mutate():
+    pass
+
+
+def init_population():
+    result = []
+
+    for x in range(NUM_AGENTS):
+        result.append(''.join(generate_random_moves(NUM_MOVES)))
+    return result
+
+
 if __name__ == '__main__':
-    m = maze(20, 30)
+
+    paths = init_population()
+
+    m = maze(20, 20)
     m.CreateMaze()
-    a = agent(m, footprints=True, filled=True)
+    a = agent(m, footprints=True, filled=True,color=COLOR.red)
     b = agent(m, footprints=True)
-    # path = ''.join(generate_random_moves(100))
+    path = ''.join(generate_random_moves(100))
     # m.tracePath({a: path, b:m.path})
+    for i in range(NUM_AGENTS):
+        m.tracePath({a: paths[i]},delay=50)
     print(m.maze_map)
     m.run()
 
