d91592e60bab68d8d99863818fc851f62da0ed9e
Vorteo <92757545+Vorteo@users.noreply.github.com>
2023-03-23 17:50:00 +0100

    Add files via upload

--- Maze/main.py
+++ Maze/main.py
@@ -1,34 +1,41 @@
 import numpy as np
-
 from pyMaze import maze, COLOR, agent
 import random
-import math
+from math import sqrt
 
-MAZE_WIDTH = 25
-MAZE_HEIGHT = 25
-NUM_MOVES = MAZE_WIDTH * MAZE_HEIGHT
-NUM_AGENTS = 100
+# -----------------------------------------------------------------------
+# Maze parameters, width and height of maze
+MAZE_WIDTH = 10
+MAZE_HEIGHT = 10
 
+# Start and goal position in maze
 START_POS = (MAZE_WIDTH, MAZE_HEIGHT)
+GOAL_POS = (1, 1)
 
+# Max number of moves in maze and number of agents
+NUM_MOVES = MAZE_WIDTH * MAZE_HEIGHT
+NUM_AGENTS = 900
+
+# Parameters for genetic algorithm
 MUTATION_RATE = 0.2
 SELECTION_CUTOFF = 0.1
 
+# Move directions == North - N, South - S, East - E, West - W
+DIRECTION_OPTIONS = ['E', 'W', 'N', 'S']
 
-directions = ['E', 'W', 'N', 'S']
 
-maze_map = None
-turn = 1
-generations = 1
+# -----------------------------------------------------------------------
 
 
 class Agent:
+    fitness = 0
+    can_walk = True
+    win = False
+
     def __init__(self):
         self.move_list = []
+        self.visited_positions = []
         self.x, self.y = START_POS
-        self.fitness = 0
-        self.can_walk = True
-        self.win = False
 
     def move(self, direction):
         if direction == 'E':
@@ -42,110 +49,167 @@ class Agent:
         else:
             print('Unknown move')
 
-    def check_next_move(self, move):
-        values = None
-        if self.can_walk:
-            if (self.x, self.y) in maze_map:
-                values = maze_map[(self.x, self.y)]
-            else:
-                self.can_walk = False
-                return
+    def next_move(self, move, maze_map, turn):
 
-            if values[move] == 1:
+        if self.can_walk is False:
+            return
+
+        next_position = tuple()
+
+        if move == 'E':
+            next_position = (self.x, self.y + 1)
+        elif move == 'W':
+            next_position = (self.x, self.y - 1)
+        elif move == 'N':
+            next_position = (self.x - 1, self.y)
+        elif move == 'S':
+            next_position = (self.x + 1, self.y)
+
+        if (self.x, self.y) in maze_map:
+            if maze_map[(self.x, self.y)][move] == 1 and next_position not in self.visited_positions:
+                self.visited_positions.append((self.x, self.y))
                 self.move(move)
             else:
-                self.can_walk = False
-                self.fitness += 350
-                return
+                if next_position in self.visited_positions:
+                    directions = [k for k, v in maze_map[(self.x, self.y)].items() if v == 1]
+                    try:
+                        directions.remove(move)
+                    except:
+                        return
+                    if len(directions) < 1:
+                        self.can_walk = False
+                        self.fitness += 200
+                        return
+                    else:
+                        tmp = random.choice(directions)
+                        self.move_list[turn] = tmp
+                        self.move(tmp)
+        else:
+            self.can_walk = False
 
 
-def generate_random_moves(num_of_steps):
-    return random.choices(directions, k=num_of_steps)
+class GeneticApplication:
+    turn = 1
+    generation = 1
+
+    def __init__(self):
+        self.maze = maze(MAZE_WIDTH, MAZE_HEIGHT)
+        self.maze.CreateMaze()
+
+        self.agents = [Agent() for _ in range(NUM_AGENTS)]
+
+    def init_population(self):
+        for i in self.agents:
+            i.move_list = generate_random_moves()
+
+    def next_generation(self, moves_list):
+        self.turn = 1
+        self.generation += 1
+
+        self.agents = [Agent() for _ in range(NUM_AGENTS)]
+        for i, m in enumerate(moves_list):
+            self.agents[i].move_list = m
+
+    def evaluate(self):
+        while True:
+
+            if self.turn == NUM_MOVES or max(a.can_walk for a in self.agents) is False:
+
+                if any(a.win for a in self.agents):
+                    print('Maze solution found')
+                    break
+
+                for a in self.agents:
+                    a.fitness = calculate_distance((a.x, a.y), GOAL_POS)
+
+                self.agents.sort(key=lambda x: x.fitness)
+                print('Best fitness of {} generation is {}'.format(self.generation, self.agents[0].fitness))
+
+                moves_list = []
+                for j in range(int(len(self.agents) * SELECTION_CUTOFF)):
+                    if j == len(self.agents):
+                        continue
+                    if len(moves_list) >= NUM_AGENTS:
+                        continue
+
+                    for k in range(j + 1, len(self.agents)):
+                        if len(moves_list) >= NUM_AGENTS:
+                            continue
+                        better_agent = self.agents[j].move_list
+                        worse_agent = self.agents[k].move_list
+                        moves_list.append(crossover(better_agent, worse_agent))
+
+                moves_list = np.array(moves_list)
+                moves_list = mutate(moves_list)
+
+                self.next_generation(moves_list)
+
+            for a in self.agents:
+                move = a.move_list[self.turn - 1]
+                a.next_move(move, self.maze.maze_map, self.turn - 1)
+
+            for a in self.agents:
+                if (a.x, a.y) == GOAL_POS:
+                    a.win = True
+                    a.can_walk = False
+                    print('Someone reach the goal destination!')
+
+            self.turn += 1
 
 
-def calculate_distance(x1, x2):
-    dist = math.dist(x1, x2)
+# -----------------------------------------------------------------------
+# Generate array of random moves Up, Down, Left and Right as North, South, East, West
+def generate_random_moves():
+    return np.array(random.choices(DIRECTION_OPTIONS, k=NUM_MOVES))
+
+
+# Calculate distance between two points
+def calculate_distance(point1, point2):
+    dist = abs(point2[0] - point1[0]) + abs(point2[1] - point1[1])
+    # dist = sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
     return dist
 
 
-def crossover(list1, list2):
-    cut_point = random.randrange(0, len(list1) - 1)
+# crossover
+def crossover(array1, array2):
+    cut_point = random.randrange(0, len(array1) - 1)
+
     if random.random() >= 0.05:
-        new_list = list1[:cut_point] + list2[cut_point:]
+        new_arr = np.concatenate((array1[:cut_point], array2[cut_point:]))
     else:
-        new_list = list2[:cut_point] + list1[cut_point:]
+        new_arr = np.concatenate((array2[:cut_point], array1[cut_point:]))
 
-    return new_list
+    return new_arr
 
 
+# mutation
 def mutate(array):
     if random.random() <= MUTATION_RATE:
         total = np.shape(array)[0] * np.shape(array)[1]
 
         amount_to_mutate = int(total * MUTATION_RATE)
-        places = [random.randint(0, total - 1) for i in range(amount_to_mutate)]
+        places = [random.randint(0, total - 1) for _ in range(amount_to_mutate)]
 
-        for ele in places:
-            row = ele / NUM_MOVES
-            column = ele % NUM_MOVES
+        for p in places:
+            row = p // NUM_MOVES
+            column = p % NUM_MOVES
             current = array[row, column]
-            array[row, column] = random.choice(directions[directions!=current])
+            array[row, column] = random.choice(DIRECTION_OPTIONS[DIRECTION_OPTIONS != current])
 
         return array
     else:
         return array
 
 
-def next_generation(moves_lists):
-    global turn, generations
-    turn = 1
-    generations += 1
-    move_array = np.array(moves_lists)
-    agents_ = [Agent() for i in range(NUM_AGENTS)]
-
-    return move_array, agents_
-
-
 if __name__ == '__main__':
+    genetic_app = GeneticApplication()
+    genetic_app.init_population()
 
-    agents = [Agent() for i in range(NUM_AGENTS)]
-    for a in agents:
-        a.move_list = generate_random_moves(NUM_MOVES)
+    genetic_app.evaluate()
 
-    m = maze(MAZE_WIDTH, MAZE_HEIGHT)
-    m.CreateMaze()
+    a_goal = [a for a in genetic_app.agents if a.win is True]
 
-    print(m.maze_map)
-    maze_map = m.maze_map
+    a = agent(genetic_app.maze, footprints=True)
+    genetic_app.maze.tracePath({a: a_goal[0].visited_positions})
 
-    a = agent(m, footprints=True, filled=False, color=COLOR.red)
-
-    while turn < NUM_MOVES or max(ag.can_walk for ag in agents) is not False:
-        for i in agents:
-            current_move = i.move_list[turn-1]
-            i.check_next_move(current_move)
-            if i.can_walk:
-                a.position = (i.x, i.y)
-        turn += 1
-
-    for i in agents:
-        i.fitness = calculate_distance((i.x, i.y), (1, 1))
-
-    agents.sort(key=lambda x: x.fitness)
-    print('Best fitness of {}'.format(agents[0].fitness))
-
-    move_lists = []
-    for j in range(agents * SELECTION_CUTOFF):
-        if j == len(agents):
-            continue
-
-        for k in range(j + 1, len(agents)):
-            better_agent = agents[j].move_list
-            worse_agent = agents[k].move_list
-            move_lists.append(crossover(better_agent, worse_agent))
-
-    move_lists = np.array(move_lists)
-    move_lists = mutate(move_lists)
-
-    m.run()
-
+    genetic_app.maze.run()
